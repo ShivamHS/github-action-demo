b8f2a48910671ddd289c1dc477f6b440cefe3968
Shivam <shivampant0998@gmail.com>
2023-08-31 17:06:34 +0530

    first commit

---
+++ github-action-demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace github_action_demo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
